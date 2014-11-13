6d6d535bf536a5ef60e050ace44ce94fcf1e89a7
Scott Arciszewski <scott@arciszewski.me>
2014-11-13 12:17:17 -0500

    Create SAFE.php
    Demo script for SAFE

---
+++ demo/Security/SAFE.php
@@ -0,0 +1,30 @@
+<?php
+define('BASE', dirname(dirname(__DIR__)));
+require_once BASE."/autoload.php";
+
+$key = 'MtDK9NAiJ4Gi/WheP9/39w==';
+$plain = 'YELLOW SUBMARINE';
+
+$msg = \Resonantcore\Lib\Security\SAFE::encrypt($plain, $key);
+
+echo $msg;
+echo "\n";
+
+$dec = \Resonantcore\Lib\Security\SAFE::decrypt($msg, $key);
+
+echo $dec;
+echo "\n";
+
+/* THIS SHOULD FAIL */
+try {
+    $idx = strlen($msg) - mt_rand(0,31);
+    $end = hexdec($msg[$idx]);
+    $end += 8;
+    $end %= 16;
+    $msg[$idx] = dechex($end);
+    
+    $dec = \Resonantcore\Lib\Security\SAFE::decrypt($msg, $key);
+    echo "OOPS! Invalid MAC didn't throw an exception. :( \n";
+} catch (Exception $e) {
+    echo "An intentionally bad MAC failed to validate.\n";
+}
