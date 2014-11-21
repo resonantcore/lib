2ae86d208300f0666538909125d2dc84437cbfbd
Scott Arciszewski <scott.arciszewski@bookthatdoc.com>
2014-11-20 23:37:18 -0500

    Add a simple utility class for some misc features we may decide to use.

---
+++ src/Utility.php
@@ -0,0 +1,26 @@
+<?php
+namespace Resonantcore\Lib;
+
+class Utility
+{
+    public static function getJSON($file)
+    {
+        if (\is_readable($file)) {
+            return self::parseJSON(\file_get_contents($file));
+        }
+    }
+
+    public static function parseJSON($json, $assoc = false, $depth = 512, $options = 0)
+    {
+        return json_decode(
+            preg_replace(
+                "#(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)|([\s\t]//.*)|(^//.*)#",
+                '',
+                $json
+            ),
+            $assoc,
+            $depth,
+            $options
+        );
+    }
+}
